895da84d5e7ff51da5c3c353fb7b4a6bd3ec0c38
unknown <sushant.shekhar151997@gmail.com>
2023-09-02 12:30:06 +0530

    lauching soon page created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+
+import { Box, Button, Grid, GridItem, Image, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react';
 import './App.css';
+import CountdownTimer from './components/CountdownTimer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box backgroundColor={'#f48221'}>
+    <Box position={'relative'} display={{base:'none',md:'block',lg:'block'}}>
+      <Grid gridTemplateColumns={'30% 70%'} paddingTop={'20px'} paddingRight={'1px'}>
+        <GridItem><Image src="./milanlogo.png" alt="milanlogo"/></GridItem>
+        <GridItem><Image src="./coverimage.webp" alt="milancoverimage"/></GridItem>
+
+      </Grid>
+      
+      <Image  src="./lauchsoonbanner.png"  alt="launch soon banner" width={'500px'} position={'absolute'} top={'160px'} left={'140px'}/>
+      <Box position={'absolute'} top={'330px'} left={'160px'}>
+      <CountdownTimer/>
+      </Box>
+      <Box position={'absolute'} bottom={'10px'} right={'10px'}>
+      <a href='#notifyme'>  <Image src="./arrowbutton.png" alt="arrow button" width={'100px'}/></a>
+      
+      </Box>
+      </Box>
+  
+      <Box display={{base:'block',md:'none',lg:'none'}} >
+      <Image src="./coverimage.webp" alt="milancoverimage"/>
+      <Image src="./milanlogo.png" alt="milanlogo" marginTop={'25px'}/>
+        <CountdownTimer/>
+      </Box>
+    <Box marginTop={{base:'25px',lg:'70px'}} paddingBottom={'50px'} id='notifyme'>
+    <Text as={'h1'} color={'white'} fontSize={{base:'18px',lg:'30px'}} fontFamily={'calibri'} marginBottom={{base:'10px',lg:'10px'}} textAlign={'center'}>We’ll let you know when we are Launching</Text>
+    <form action="mailto:info@nodust.in" method="post" enctype="text/plain" autoComplete='off'>
+      <InputGroup width={{base:'80%',md:'70%',lg:'600px'}} margin={'auto'}>
+       
+      <Input
+        type="email"
+        name="email"
+        backgroundColor={'white'}
+        placeholder="Enter your Email..."
+        fontWeight={'bold'}
+        color="black" // Text color
+        borderRadius="5px" // Border radius
+        required
+      />
+      <InputRightElement width="100px">
+        <Button
+        type="Submit"
+        value={'Notify Me'}
+          bgGradient="linear(to-r, orange.400, green.400)"
+          borderRadius={'0px 5px 5px 0px'}
+          color="white" // Button text color
+         _hover={
+         {
+          cursor:'pointer'
+         }
+         }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+         Notify Me
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+    </form>
+    </Box>
+   
+      </Box>
   );
 }
 
---
+++ src/components/CountdownTimer.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+ // Create this CSS file for styling
+import '../App.css';
+const CountdownTimer = () => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  function calculateTimeLeft() {
+    const targetDate = new Date('2023-09-20T23:59:59');
+    const now = new Date();
+    const difference = targetDate - now;
+
+    if (difference <= 0) {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return {
+      days: days.toString().padStart(2, '0'),
+      hours: hours.toString().padStart(2, '0'),
+      minutes: minutes.toString().padStart(2, '0'),
+      seconds: seconds.toString().padStart(2, '0'),
+    };
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className="countdown-container">
+      <div className="countdown-item DigitalNumbers" id="days">
+        {timeLeft.days}
+        <span style={{fontFamily:'calibri'}}>Days</span>
+      </div>
+      <div className="countdown-item DigitalNumbers" id="hours">
+        {timeLeft.hours}
+        <span style={{fontFamily:'calibri'}}>Hours</span>
+      </div>
+      <div className="countdown-item DigitalNumbers" id="minutes">
+        {timeLeft.minutes}
+        <span style={{fontFamily:'calibri'}}>Minutes</span>
+      </div>
+      <div className="countdown-item DigitalNumbers" id="seconds">
+        {timeLeft.seconds}
+        <span style={{fontFamily:'calibri'}}>Seconds</span>
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
